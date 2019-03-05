f9cce57befcfdc3ef3742b099b5e6cf94c298b74
Sergey Litskevich <litskevich-ss@users.noreply.github.com>
2019-03-05 09:43:14 +0700

    Исправил позиционирование кнопки прокрутки страницы вверх

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,20 @@ close.addEventListener('click', function () {
 });*/
 
 
-$('#up').click(function () {
+/* Плавно прокручиваем страницу вверх*/
+$('#upTop').click(function () {
   $('html, body').animate({ scrollTop: 0 }, 500);
   return false;
 })
+
+/* Скрываем иконку прокрутки вверх при открытии страницы*/
+$('.upTop').hide();
+
+/* Плавно отображаем иконку при прокрктке вниз и скрываем при прокрутке вверх*/
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+    $('.upTop').fadeIn();
+  } else {
+    $('.upTop').fadeOut();
+  }
+});
