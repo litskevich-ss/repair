837629ef1c612392aa1cbcffb90af773f0d46d8d
Sergey <slic@live.ru>
2019-03-12 01:05:39 +0700

    Сверстал секцию 5 шагов до ремонта и анимировал ее

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,3 @@
-/*var button = document.querySelector('#button');
-var modal = document.querySelector('#modal');
-var close = document.querySelector('#close');
-
-button.addEventListener('click', function () {
-  modal.classList.add('modal_active');
-});
-
-close.addEventListener('click', function () {
-  modal.classList.remove('modal_active');
-});*/
-
-
 /* Плавно прокручиваем страницу вверх*/
 $('#upTop').click(function () {
   $('html, body').animate({ scrollTop: 0 }, 500);
